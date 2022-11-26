1d6384c01d1892f03c01c4c6f009848e47e984f2
Firoj Alam Raju <firojalamraju85@gmail.com>
2022-11-26 18:36:49 +0600

    added product details

--- index.js
+++ index.js
@@ -47,11 +47,42 @@ async function run() {
             const result = await productsCollections.find(query).toArray();
             res.send(result);
         })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            console.log(user);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '10days' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: 'token' })
+        });
+
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await productsCollections.findOne(query)
+            res.send(service)
+        });
+
+
+
+
+        // app.get('/users/admin/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     const query = { email }
+        //     const user = await usersCollection.findOne(query);
+        //     res.send({ isAdmin: user?.role === 'buyer' });
+        // })
 
     }
     finally {
