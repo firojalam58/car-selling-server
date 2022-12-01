8258666aba3cab9c22430667315481bb5b4c4b34
Firoj Alam Raju <firojalamraju85@gmail.com>
2022-12-02 00:45:48 +0600

    implement buyers seller

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
-const { query } = require('express');
 const app = express()
 require('dotenv').config()
-
 
 
 
@@ -15,7 +13,21 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 // Mogodb Start 
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({message:'Unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, 
+        function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bjrjkm4.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,6 +40,28 @@ async function run() {
         const categoriesCollections = client.db('carProducta').collection('categories');
         const bookingCollections = client.db('carProducta').collection('bookings');
         const usersCollection = client.db('carProducta').collection('users')
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '5d' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
+
+        const verifyAdmin = async (req, res, next) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'Admin') {
+                return res.status(403).send({ message: 'Forbidden Access' })
+            }
+            next();
+        }
+
         app.get('/products', async (req, res) => {
             const query = {};
             const products = await productsCollections.find(query).toArray()
@@ -108,14 +142,61 @@ async function run() {
             res.send(result)
         });
 
-        // app.get('/users/admin/:email', async (req, res) => {
-        //     const email = req.params.email;
-        //     const query = { email }
-        //     const user = await usersCollection.findOne(query);
-        //     res.send({ isAdmin: user?.role === 'buyer' });
-        // })
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        })
+      
+
+       
+
+        
+        //  for all sellers
+        app.get('/allsellers', async (req, res) => {
+            const query = { role: "Seller" };
+            const sellers = await usersCollection.find(query).toArray();
+            res.send(sellers);
+        })
+
+        app.get('/allsellers/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.findOne(query);
+            res.send(result);
+
+        });
+        app.delete('/allsellers/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+
+        });
+        app.get('/allbuyers', async (req, res) => {
+            const query = {role: 'Buyer'};
+            const result = await usersCollection.find(query).toArray();
+            res.send(result);
+
+        });
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            if(user.role === 'Admin'){
+              return  res.send(user);
+            }
+            res.status(401).send({message: 'Unauthorize Access'})
+        });
+
+
+
 
     }
+
+
+
     finally {
 
     }
